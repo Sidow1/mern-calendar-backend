9c415a60d7720e87c5d1b3293dde5867dcccb186
Thomas Chavez <thomas.chavezc@utem.cl>
2023-03-25 12:49:36 -0300

    Commit para deploy

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ app.use(express.json());
 // Rutas
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
-// TODO: CRUD
 
 // Escuchar peticiones
 app.listen(process.env.PORT, () => {
